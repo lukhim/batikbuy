838a15f89c5aa93708220523234444e115153de0
lukhim <lukmanhak02@gmail.com>
2022-12-19 09:45:14 +0700

    database batikbuy import dulu ke hostingannya

--- database/batikbuy.sql
+++ database/batikbuy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 18, 2022 at 02:16 PM
+-- Generation Time: Dec 19, 2022 at 03:44 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -39,7 +39,7 @@ CREATE TABLE `menu` (
 --
 
 CREATE TABLE `produk` (
-  `id_produk` int(10) NOT NULL,
+  `id` int(10) NOT NULL,
   `nama_batik` varchar(100) NOT NULL,
   `motif` varchar(100) NOT NULL,
   `asal_daerah` varchar(100) NOT NULL,
@@ -47,17 +47,19 @@ CREATE TABLE `produk` (
   `bahan` varchar(50) NOT NULL,
   `harga` varchar(20) NOT NULL,
   `stok` int(3) NOT NULL,
-  `gender` varchar(15) NOT NULL
+  `gender` varchar(15) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id_produk`, `nama_batik`, `motif`, `asal_daerah`, `size`, `bahan`, `harga`, `stok`, `gender`) VALUES
-(1, 'Kemeja Pria Batik Sentani', 'Motif Sentani', 'Papua', 'S – M – L – XL', 'Katun Premium', 'Rp 175.000', 9, 'Pria'),
-(2, 'Kemeja Pria Batik Abimanyu', 'Motif Abimanyu', 'Jawa Tengah dan Yogyakarta', 'S – M – L – XL', 'Katun Premium', 'Rp 150.000', 7, 'Pria'),
-(3, 'Blouse Wanita Batik Solo', 'Motif Daun Pagar', 'Solo', 'S – M – L – XL', 'Katun Cap Garutan', 'Rp 200.000', 10, 'Wanita');
+INSERT INTO `produk` (`id`, `nama_batik`, `motif`, `asal_daerah`, `size`, `bahan`, `harga`, `stok`, `gender`, `image`) VALUES
+(1, 'Kemeja Pria Batik Sentani', 'Motif Sentani', 'Papua', 'S – M – L – XL', 'Katun Premium', 'Rp 175.000', 9, 'Pria', NULL),
+(2, 'Kemeja Pria Batik Abimanyu', 'Motif Abimanyu', 'Jawa Tengah dan Yogyakarta', 'S – M – L – XL', 'Katun Premium', 'Rp 150.000', 7, 'Pria', NULL),
+(3, 'Blouse Wanita Batik Solo', 'Motif Daun Pagar', 'Solo', 'S – M – L – XL', 'Katun Cap Garutan', 'Rp 200.000', 10, 'Wanita', NULL),
+(4, 'Kemeja  Pria Batik Arya Cakrawala', 'Motif Arya Cakrawala', 'Jawa', 'S-M-L-XL', 'Katun Premium', 'Rp. 180.000', 10, 'Pria', '');
 
 -- --------------------------------------------------------
 
@@ -69,6 +71,14 @@ CREATE TABLE `role` (
   `id` int(11) NOT NULL,
   `role` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`id`, `role`) VALUES
+(1, 'administrator'),
+(2, 'member');
 
 -- --------------------------------------------------------
 
@@ -92,7 +102,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `nama`, `email`, `image`, `password`, `role_id`, `is_active`, `tanggal_input`) VALUES
-(17, 'myman', 'myman@damn.org', 'default.jpg', '$2y$10$ef25AQbvFHd7EAoCt8GwjOSlGLP47sY8eTWjNKVC4xTxNyPG6yNxq', 2, 1, 1671368973);
+(17, 'myman', 'myman@damn.org', 'pro1671372583.png', '$2y$10$ef25AQbvFHd7EAoCt8GwjOSlGLP47sY8eTWjNKVC4xTxNyPG6yNxq', 1, 1, 1671368973),
+(18, 'lukmanul hakim', 'lukman@gmail.com', 'default.jpg', '$2y$10$DMFt1lDcec5yQizQbmHDZ.LezZ3u9m3YTnvqua5lup1Wf9O5bAQIm', 2, 1, 1671412656);
 
 --
 -- Indexes for dumped tables
@@ -108,7 +119,7 @@ ALTER TABLE `menu`
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
-  ADD PRIMARY KEY (`id_produk`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `role`
@@ -136,7 +147,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `id_produk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `role`
@@ -148,7 +159,7 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
